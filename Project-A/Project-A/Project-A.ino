cc8ecd8ac40b4d0d5471fc9cb1eeee9bccab824c
xrayhunter <xrayhunter8@gmail.com>
2018-03-13 19:30:38 -0700

    Update 0.0.1

--- Project-A/Project-A/Project-A.ino
+++ Project-A/Project-A/Project-A.ino
@@ -4,14 +4,69 @@
  Author:	Brandon Smith, and Ray Smith
 */
 
+#include "lib\threads\Thread.h"
+#include "lib\lcd\LiquidCrystal.h"
+
+Thread* lcdThread;
+LiquidCrystal lcd;
+
+int lcdCounter = -1;
+
+int batteryVoltage = 0;
+int batteryAC = 0;
+
 // the setup function runs once when you press reset or power the board
 void setup() 
 {
-	
+	Serial.begin(9600);
+	// Initialize instance data
+	lcdThread = new Thread();
+	lcd = LiquidCrystal(12, 11, 5, 4, 3, 2);
+	lcd.begin(16, 2);
+
+	// Setup lcdThread
+	lcdThread->setInterval(5000);
+	lcdThread->enabled = true;
+	lcdThread->onRun(onLCDUpdate);
+	if (lcdThread->shouldRun())
+	{
+		lcdThread->run();
+	}
+	else
+	{
+		Serial.println("[Chip Thread | Critical Error]: Failed to open LCD Thread.");
+	}
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() 
 {
   
+}
+
+
+void onLCDUpdate()
+{
+	if (lcdCounter == -1)
+	{
+		lcd.setCursor(0, 0);
+		lcd.println("		Board Initializing...");
+		delay(500);
+		lcd.clear();
+
+		lcdCounter++;
+		onLCDUpdate();
+		return;
+	}
+
+	if (lcdCounter == 0)
+	{
+
+	}
+
+	lcdCounter++;
+	if (lcdCounter >= 5)
+	{
+		lcdCounter = 0;
+	}
 }
