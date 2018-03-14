fc521da0223a431c39997f92f5f9aca1c2ad8e74
xrayhunter <xrayhunter@hotmail.com>
2018-03-13 18:32:42 -0700

    Setup

---
+++ Project-A/Project-A/Project-A.ino
@@ -0,0 +1,17 @@
+/*
+ Name:		Project_A.ino
+ Created:	3/13/2018 6:29:22 PM
+ Author:	Brandon Smith, and Ray Smith
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() 
+{
+	
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() 
+{
+  
+}
